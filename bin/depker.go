80ea204c2ac3be3d751269c83efe1a9968fbf71e
syfxlin <i@ixk.me>
2022-07-10 15:58:59 +0800

    feature: Add version support

--- bin/depker.go
+++ bin/depker.go
@@ -22,48 +22,51 @@ func run(name string, command []string) {
 	}
 }
 
-func version(version string) string {
+func version(version string) (bool, string) {
 	if version == "master" {
-		return "master"
+		return true, version
 	}
 	ok, err := regexp.MatchString("v\\d+\\.\\d+\\.\\d+(-beta\\d+|-alpha\\d+)?", version)
 	if err != nil {
 		panic(err)
 	}
 	if ok {
-		return version
+		return true, version
 	} else {
-		return ""
+		return false, "master"
 	}
 }
 
 func main() {
 	// proxy
-	if os.Args[1] == "deno" {
+	if len(os.Args) > 1 && os.Args[1] == "deno" {
 		run("deno", os.Args[2:])
 		return
 	}
-	if os.Args[1] == "docker" {
+	if len(os.Args) > 1 && os.Args[1] == "docker" {
 		run("docker", os.Args[2:])
 		return
 	}
 
 	// command
-	if os.Args[1] == "update" {
-		version := version(os.Args[2])
-		if version == "" {
-			run("deno", []string{"cache", "-r", fmt.Sprintf(mod, "master")})
-		} else {
-			run("deno", []string{"cache", "-r", fmt.Sprintf(mod, version)})
+	if len(os.Args) > 1 && os.Args[1] == "update" {
+		ver := "master"
+		if len(os.Args) > 2 {
+			_, ver = version(os.Args[2])
 		}
+		run("deno", []string{"cache", "-r", fmt.Sprintf(mod, ver)})
 		return
 	}
 
 	// depker
-	version := version(os.Args[1])
-	if version == "" {
-		run("deno", append([]string{"run", "-q", "-A", fmt.Sprintf(mod, "master")}, os.Args[1:]...))
+	ok := false
+	ver := "master"
+	if len(os.Args) > 1 {
+		ok, ver = version(os.Args[1])
+	}
+	if ok {
+		run("deno", append([]string{"run", "-q", "-A", fmt.Sprintf(mod, ver)}, os.Args[2:]...))
 	} else {
-		run("deno", append([]string{"run", "-q", "-A", fmt.Sprintf(mod, version)}, os.Args[2:]...))
+		run("deno", append([]string{"run", "-q", "-A", fmt.Sprintf(mod, ver)}, os.Args[1:]...))
 	}
 }
