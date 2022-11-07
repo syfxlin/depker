c1a0f8532f14366e61129ed077059d5f6e955b2f
syfxlin <i@ixk.me>
2022-11-07 23:28:57 +0800

    feature: update components

--- packages/depker-ui/src/pages/AppList.tsx
+++ packages/depker-ui/src/pages/AppList.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent } from "react";
+import React, { ChangeEvent, useMemo } from "react";
 import { Main } from "../components/layout/Main";
 import {
   ActionIcon,
@@ -26,131 +26,139 @@ import { client } from "../api/client";
 export const AppList: React.FC = () => {
   const t = useMantineTheme();
   const apps = useApps();
+
+  const Search = useMemo(
+    () => (
+      <Input
+        value={apps.values.search}
+        onChange={(e: ChangeEvent<HTMLInputElement>) => apps.update.search(e.target.value)}
+        placeholder="Search apps"
+        icon={<TbSearch />}
+        rightSection={
+          apps.values.search ? (
+            <ActionIcon onClick={() => apps.update.search("")}>
+              <TbX />
+            </ActionIcon>
+          ) : (
+            <Text />
+          )
+        }
+      />
+    ),
+    [apps.values.search]
+  );
+
+  const List = useMemo(
+    () =>
+      apps.data && (
+        <Pages
+          edges
+          page={apps.values.page}
+          size={apps.values.size}
+          total={apps.data.total}
+          onChange={apps.update.page}
+        >
+          <Grid>
+            {apps.data?.items?.map((item) => (
+              <Grid.Col key={`apps-${item.name}`} span={4}>
+                <Card
+                  withBorder
+                  css={css`
+                    padding: ${t.spacing.md}px ${t.spacing.lg}px;
+                    border-radius: ${t.radius.sm}px;
+                    overflow: visible;
+                  `}
+                >
+                  <Group>
+                    <Avatar src={client.asset.icon(item.icon)}>
+                      <TbApiApp />
+                    </Avatar>
+                    <Link
+                      to={`/apps/${item.name}`}
+                      css={css`
+                        flex: 1;
+                        text-decoration: none;
+                        width: 100%;
+                        overflow: hidden;
+
+                        .mantine-Badge-root {
+                          text-transform: none;
+                          text-overflow: ellipsis;
+                          max-width: 100%;
+                        }
+                      `}
+                    >
+                      <Text
+                        css={css`
+                          display: block;
+                          color: ${t.fn.primaryColor()};
+                          font-size: ${t.fontSizes.xl}px;
+                          font-weight: 500;
+                          text-decoration: none;
+                          margin-bottom: ${t.spacing.xs * 0.5}px;
+                        `}
+                      >
+                        {item.name}
+                      </Text>
+                      {item.buildpack && (
+                        <Text size="xs" color="dimmed">
+                          Buildpack: {item.buildpack}
+                        </Text>
+                      )}
+                      <Text size="xs" color="dimmed">
+                        Domain: {item.domain || "No configuration"}
+                      </Text>
+                      <Tooltip
+                        label={
+                          <Text
+                            css={css`
+                              font-size: ${t.fontSizes.xs}px;
+                              font-family: ${t.fontFamilyMonospace};
+                            `}
+                          >
+                            Deployd in {DateTime.fromMillis(item.deploydAt).toLocaleString(DateTime.DATETIME_SHORT)}
+                            <br />
+                            Created in {DateTime.fromMillis(item.createdAt).toLocaleString(DateTime.DATETIME_SHORT)}
+                            <br />
+                            Updated in {DateTime.fromMillis(item.updatedAt).toLocaleString(DateTime.DATETIME_SHORT)}
+                          </Text>
+                        }
+                      >
+                        <Text size="xs" color="dimmed">
+                          Uptime:{" "}
+                          {item.deploydAt === 0
+                            ? DateTime.fromMillis(item.deploydAt).toLocaleString(DateTime.DATETIME_SHORT)
+                            : "No deployment"}
+                        </Text>
+                      </Tooltip>
+                    </Link>
+                    <Badge color={colors[item.status]}>{item.status}</Badge>
+                    {item.domain && item.domain.length && (
+                      <ActionIcon color={t.primaryColor} size="lg" onClick={() => window.open(`http://${item.domain}`)}>
+                        <TbArrowUpRight />
+                      </ActionIcon>
+                    )}
+                  </Group>
+                </Card>
+              </Grid.Col>
+            ))}
+          </Grid>
+        </Pages>
+      ),
+    [apps.data, apps.values.page, apps.values.size, apps.values.sort]
+  );
+
   return (
     <Main
       title="Apps"
       header={
         <Group>
-          <Input
-            value={apps.values.search}
-            onChange={(e: ChangeEvent<HTMLInputElement>) => apps.update.search(e.target.value)}
-            placeholder="Search apps"
-            icon={<TbSearch />}
-            rightSection={
-              apps.values.search ? (
-                <ActionIcon onClick={() => apps.update.search("")}>
-                  <TbX />
-                </ActionIcon>
-              ) : (
-                <Text />
-              )
-            }
-          />
+          {Search}
           <Button>New App</Button>
         </Group>
       }
     >
-      <Async query={apps.query}>
-        {apps.data && (
-          <Pages
-            edges
-            page={apps.values.page}
-            size={apps.values.size}
-            total={apps.data.total}
-            onChange={apps.update.page}
-          >
-            <Grid>
-              {apps.data?.items?.map((item) => (
-                <Grid.Col key={`apps-${item.name}`} span={4}>
-                  <Card
-                    withBorder
-                    css={css`
-                      padding: ${t.spacing.md}px ${t.spacing.lg}px;
-                      border-radius: ${t.radius.sm}px;
-                      overflow: visible;
-                    `}
-                  >
-                    <Group>
-                      <Avatar src={client.asset.icon(item.icon)}>
-                        <TbApiApp />
-                      </Avatar>
-                      <Link
-                        to={`/apps/${item.name}`}
-                        css={css`
-                          flex: 1;
-                          text-decoration: none;
-                          width: 100%;
-                          overflow: hidden;
-
-                          .mantine-Badge-root {
-                            text-transform: none;
-                            text-overflow: ellipsis;
-                            max-width: 100%;
-                          }
-                        `}
-                      >
-                        <Text
-                          css={css`
-                            display: block;
-                            color: ${t.fn.primaryColor()};
-                            font-size: ${t.fontSizes.xl}px;
-                            font-weight: 500;
-                            text-decoration: none;
-                            margin-bottom: ${t.spacing.xs * 0.5}px;
-                          `}
-                        >
-                          {item.name}
-                        </Text>
-                        {item.buildpack && (
-                          <Text size="xs" color="dimmed">
-                            Buildpack: {item.buildpack}
-                          </Text>
-                        )}
-                        <Text size="xs" color="dimmed">
-                          Domain: {item.domain || "No configuration"}
-                        </Text>
-                        <Tooltip
-                          label={
-                            <Text
-                              css={css`
-                                font-size: ${t.fontSizes.xs}px;
-                                font-family: ${t.fontFamilyMonospace};
-                              `}
-                            >
-                              Deployd in {DateTime.fromMillis(item.deploydAt).toLocaleString(DateTime.DATETIME_SHORT)}
-                              <br />
-                              Created in {DateTime.fromMillis(item.createdAt).toLocaleString(DateTime.DATETIME_SHORT)}
-                              <br />
-                              Updated in {DateTime.fromMillis(item.updatedAt).toLocaleString(DateTime.DATETIME_SHORT)}
-                            </Text>
-                          }
-                        >
-                          <Text size="xs" color="dimmed">
-                            Uptime:{" "}
-                            {item.deploydAt === 0
-                              ? DateTime.fromMillis(item.deploydAt).toLocaleString(DateTime.DATETIME_SHORT)
-                              : "No deployment"}
-                          </Text>
-                        </Tooltip>
-                      </Link>
-                      <Badge color={colors[item.status]}>{item.status}</Badge>
-                      {item.domain && item.domain.length && (
-                        <ActionIcon
-                          color={t.primaryColor}
-                          size="lg"
-                          onClick={() => window.open(`http://${item.domain}`)}
-                        >
-                          <TbArrowUpRight />
-                        </ActionIcon>
-                      )}
-                    </Group>
-                  </Card>
-                </Grid.Col>
-              ))}
-            </Grid>
-          </Pages>
-        )}
-      </Async>
+      <Async query={apps.query}>{List}</Async>
     </Main>
   );
 };
--- packages/depker-ui/src/pages/AppSetting.tsx
+++ packages/depker-ui/src/pages/AppSetting.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { Main } from "../components/layout/Main";
 import { Outlet, useNavigate, useParams } from "react-router-dom";
 import { Badge, Button, Grid, Group, Loader, Stack } from "@mantine/core";
@@ -16,13 +16,13 @@ import {
   TbTerminal,
   TbWreckingBall,
 } from "react-icons/all";
-import { NavLink } from "../components/core/NavLink";
 import { colors, useStatus } from "../api/use-status";
 import { useApp } from "../api/use-app";
 import { showNotification } from "@mantine/notifications";
 import { error } from "../utils/message";
 import { useLoading } from "../hooks/use-loading";
 import { css } from "@emotion/react";
+import { NavLink } from "../components/core/NavLink";
 
 export type AppSettingContext = {
   name: string;
@@ -37,134 +37,184 @@ export const AppSetting: React.FC = () => {
   const status = useStatus(name!);
   const running = useLoading();
 
+  const Status = useMemo(
+    () => (
+      <Badge
+        size="lg"
+        color={colors[status.data]}
+        leftSection={status.loading && <Loader size="xs" color={colors[status.data]} />}
+        css={css`
+          .mantine-Badge-leftSection {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+          }
+        `}
+      >
+        {status.data}
+      </Badge>
+    ),
+    [status.data, status.loading]
+  );
+
+  const Stop = useMemo(
+    () =>
+      ["running", "restarting", "exited"].includes(status.data) && (
+        <Button
+          loading={running.value}
+          variant="light"
+          color="red"
+          leftIcon={<TbPlayerPause />}
+          onClick={() => {
+            (async () => {
+              try {
+                running.update(true);
+                await app.actions.stop();
+                running.update(false);
+                showNotification({
+                  title: "Stop successful",
+                  message: `Application stop successful.`,
+                  color: "green",
+                });
+              } catch (e: any) {
+                showNotification({
+                  title: "Stop failure",
+                  message: error(e),
+                });
+              }
+            })();
+          }}
+        >
+          Stop
+        </Button>
+      ),
+    [status.data, running.value]
+  );
+
+  const Restart = useMemo(
+    () =>
+      ["running", "restarting", "exited"].includes(status.data) && (
+        <Button
+          loading={running.value}
+          variant="light"
+          leftIcon={<TbRefresh />}
+          onClick={() => {
+            (async () => {
+              try {
+                running.update(true);
+                await app.actions.restart();
+                running.update(false);
+                showNotification({
+                  title: "Restart successful",
+                  message: `Application restart successful.`,
+                  color: "green",
+                });
+              } catch (e: any) {
+                showNotification({
+                  title: "Restart failure",
+                  message: error(e),
+                });
+              }
+            })();
+          }}
+        >
+          Restart
+        </Button>
+      ),
+    [status.data, running.value]
+  );
+
+  const Deploy = useMemo(
+    () => (
+      <Button
+        loading={running.value}
+        variant="light"
+        leftIcon={status.data !== "stopped" ? <TbPlayerStop /> : <TbPlayerPlay />}
+        onClick={() => {
+          (async () => {
+            try {
+              running.update(true);
+              const deploy = await app.actions.deploy(status.data !== "stopped");
+              running.update(false);
+              showNotification({
+                title: "Deploy successful",
+                message: `Application create deploy #${deploy.id} successful.`,
+                color: "green",
+              });
+              navigate(`/apps/depker/deploys/${deploy.id}`);
+            } catch (e: any) {
+              showNotification({
+                title: "Deploy failure",
+                message: error(e),
+              });
+            }
+          })();
+        }}
+      >
+        {status.data !== "stopped" ? "Re-deploy" : "Deploy"}
+      </Button>
+    ),
+    [status.data, running.value]
+  );
+
+  const Save = useMemo(
+    () => (
+      <Button
+        loading={running.value}
+        leftIcon={<TbDeviceFloppy />}
+        onClick={() => {
+          (async () => {
+            try {
+              running.update(true);
+              await app.actions.save();
+              running.update(false);
+              showNotification({
+                title: "Save successful",
+                message: "Application save successful.",
+                color: "green",
+              });
+            } catch (e: any) {
+              showNotification({
+                title: "Save failure",
+                message: error(e),
+              });
+            }
+          })();
+        }}
+      >
+        Save
+      </Button>
+    ),
+    [running.value]
+  );
+
+  const AppNav = useMemo(
+    () => (
+      <Grid.Col span={12} md={3}>
+        <Stack spacing="xs">
+          <NavLink label="Configs" icon={<TbInfoCircle />} action={`/apps/${name}/`} />
+          <NavLink label="Deploys" icon={<TbWreckingBall />} action={`/apps/${name}/deploys`} />
+          <NavLink label="Metrics" icon={<TbActivity />} action={`/apps/${name}/metrics`} />
+          <NavLink label="Logs" icon={<TbNotes />} action={`/apps/${name}/logs`} />
+          <NavLink label="Terminal" icon={<TbTerminal />} action={`/apps/${name}/terminal`} />
+          <NavLink label="History" icon={<TbHistory />} action={`/apps/${name}/history`} />
+          <NavLink label="Danger Zone" icon={<TbAlertTriangle />} color="red" action={`/apps/${name}/danger`} />
+        </Stack>
+      </Grid.Col>
+    ),
+    [name]
+  );
+
   return (
     <Main
       title="App Settings"
       header={
         <Group spacing="xs">
-          <Badge
-            size="lg"
-            color={colors[status.data]}
-            leftSection={status.loading && <Loader size="xs" color={colors[status.data]} />}
-            css={css`
-              .mantine-Badge-leftSection {
-                display: flex;
-                justify-content: center;
-                align-items: center;
-              }
-            `}
-          >
-            {status.data}
-          </Badge>
-          {["running", "restarting", "exited"].includes(status.data) && (
-            <Button
-              loading={running.value}
-              variant="light"
-              color="red"
-              leftIcon={<TbPlayerPause />}
-              onClick={() => {
-                (async () => {
-                  try {
-                    running.update(true);
-                    await app.actions.stop();
-                    running.update(false);
-                    showNotification({
-                      title: "Stop successful",
-                      message: `Application stop successful.`,
-                      color: "green",
-                    });
-                  } catch (e: any) {
-                    showNotification({
-                      title: "Stop failure",
-                      message: error(e),
-                    });
-                  }
-                })();
-              }}
-            >
-              Stop
-            </Button>
-          )}
-          {["running", "restarting", "exited"].includes(status.data) && (
-            <Button
-              loading={running.value}
-              variant="light"
-              leftIcon={<TbRefresh />}
-              onClick={() => {
-                (async () => {
-                  try {
-                    running.update(true);
-                    await app.actions.restart();
-                    running.update(false);
-                    showNotification({
-                      title: "Restart successful",
-                      message: `Application restart successful.`,
-                      color: "green",
-                    });
-                  } catch (e: any) {
-                    showNotification({
-                      title: "Restart failure",
-                      message: error(e),
-                    });
-                  }
-                })();
-              }}
-            >
-              Restart
-            </Button>
-          )}
-          <Button
-            loading={running.value}
-            variant="light"
-            leftIcon={status.data !== "stopped" ? <TbPlayerStop /> : <TbPlayerPlay />}
-            onClick={() => {
-              (async () => {
-                try {
-                  running.update(true);
-                  const deploy = await app.actions.deploy(status.data !== "stopped");
-                  running.update(false);
-                  showNotification({
-                    title: "Deploy successful",
-                    message: `Application create deploy #${deploy.id} successful.`,
-                    color: "green",
-                  });
-                  navigate(`/apps/depker/deploys/${deploy.id}`);
-                } catch (e: any) {
-                  showNotification({
-                    title: "Deploy failure",
-                    message: error(e),
-                  });
-                }
-              })();
-            }}
-          >
-            {status.data !== "stopped" ? "Re-deploy" : "Deploy"}
-          </Button>
-          <Button
-            loading={running.value}
-            leftIcon={<TbDeviceFloppy />}
-            onClick={() => {
-              (async () => {
-                try {
-                  running.update(true);
-                  await app.actions.save();
-                  running.update(false);
-                  showNotification({
-                    title: "Save successful",
-                    message: "Application save successful.",
-                    color: "green",
-                  });
-                } catch (e: any) {
-                  showNotification({
-                    title: "Save failure",
-                    message: error(e),
-                  });
-                }
-              })();
-            }}
-          >
-            Save
-          </Button>
+          {Status}
+          {Stop}
+          {Restart}
+          {Deploy}
+          {Save}
         </Group>
       }
     >
@@ -173,17 +223,7 @@ export const AppSetting: React.FC = () => {
           flex: 1;
         `}
       >
-        <Grid.Col span={12} md={3}>
-          <Stack spacing="xs">
-            <NavLink label="Configs" icon={<TbInfoCircle />} action={`/apps/${name}/`} />
-            <NavLink label="Deploys" icon={<TbWreckingBall />} action={`/apps/${name}/deploys`} />
-            <NavLink label="Metrics" icon={<TbActivity />} action={`/apps/${name}/metrics`} />
-            <NavLink label="Logs" icon={<TbNotes />} action={`/apps/${name}/logs`} />
-            <NavLink label="Terminal" icon={<TbTerminal />} action={`/apps/${name}/terminal`} />
-            <NavLink label="History" icon={<TbHistory />} action={`/apps/${name}/history`} />
-            <NavLink label="Danger Zone" icon={<TbAlertTriangle />} color="red" action={`/apps/${name}/danger`} />
-          </Stack>
-        </Grid.Col>
+        {AppNav}
         <Grid.Col span={12} md={9}>
           <Outlet context={{ name, app, status }} />
         </Grid.Col>
