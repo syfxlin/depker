c0b5490a8cfcbce2965e9f9d8ff2c729a18b8d02
syfxlin <i@ixk.me>
2022-11-19 20:15:03 +0800

    feature: update git server authenticate

--- packages/depker-server/src/controllers/git.controller.ts
+++ packages/depker-server/src/controllers/git.controller.ts
@@ -1,51 +1,52 @@
-import { Controller, Logger, NotFoundException, UnauthorizedException } from "@nestjs/common";
+import { Controller, Logger } from "@nestjs/common";
 import { FetchData, Git, PushData } from "node-git-server";
 import { HttpAdapterHost } from "@nestjs/core";
-import { compareSync } from "bcrypt";
 import { Express } from "express";
 import { PATHS } from "../constants/depker.constant";
-import { Setting } from "../entities/setting.entity";
 import { App } from "../entities/app.entity";
+import { AuthService } from "../guards/auth.service";
 
-@Controller()
+@Controller("/api/repos")
 export class GitController {
   private readonly logger = new Logger(GitController.name);
   private readonly git: Git;
 
-  constructor(private readonly adapter: HttpAdapterHost) {
-    this.git = new Git(PATHS.REPOS, {
-      autoCreate: true,
-      authenticate: (options, next) => {
-        options.user(async (username, password) => {
-          try {
-            const setting = await Setting.read();
-            if (!username || !password || username !== setting.username || !compareSync(password, setting.password)) {
-              next(new UnauthorizedException("Username or password do not match, please try again."));
-              return;
-            }
-
-            const app = await App.find({ where: { name: options.repo } });
-            if (!app) {
-              next(new NotFoundException("Application not found, should be create before push."));
-              return;
-            }
-
-            next();
-          } catch (e) {
-            this.logger.error(`Git authenticate error.`, e);
-            next(e as Error);
-          }
-        });
-      },
-    });
+  constructor(private readonly adapter: HttpAdapterHost, private readonly auths: AuthService) {
+    const app = this.adapter.httpAdapter.getInstance<Express>();
+    this.git = new Git(PATHS.REPOS, { autoCreate: true });
 
     this.git.on("push", (data) => this.onPush(data));
     this.git.on("fetch", (data) => this.onFetch(data));
 
-    const app = this.adapter.httpAdapter.getInstance<Express>();
-    app.all("/repos/*", (req, res) => {
-      req.url = req.url?.replace(/^\/repos\//, "/");
-      this.git.handle(req, res);
+    app.all("/api/repos/*", async (request, response) => {
+      // authenticate
+      try {
+        await this.auths.request(request);
+      } catch (e: any) {
+        return response
+          .status(401)
+          .header("Content-Type", "text/plain")
+          .header("WWW-Authenticate", 'Basic realm="authorization needed"')
+          .send("401 Unauthorized");
+      }
+      // find application
+      const match = request.url?.match(/^\/api\/repos\/([^/]+)\/.+/);
+      if (!match) {
+        return response
+          .status(400)
+          .header("Content-Type", "text/plain")
+          .send("Application not found, should be create before push.");
+      }
+      const count = await App.countBy({ name: match[1] });
+      if (!count) {
+        return response
+          .status(400)
+          .header("Content-Type", "text/plain")
+          .send("Application not found, should be create before push.");
+      }
+      // git server
+      request.url = request.url?.replace(/^\/api\/repos\//, "/");
+      this.git.handle(request, response);
     });
   }
 
