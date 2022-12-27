ce297a1226d04ed0bfc9d029d9a76d9e9b6dde12
syfxlin <i@ixk.me>
2022-12-27 20:35:27 +0800

    feature: complete cron events

--- packages/depker-server/src/events/cron.event.ts
+++ packages/depker-server/src/events/cron.event.ts
@@ -1,3 +1,5 @@
+import { CronHistory } from "../entities/cron-history.entity";
+
 export enum CronEvent {
   CANCEL = "cron.cancel",
   PRE_START = "cron.pre_start",
@@ -10,4 +12,10 @@ export enum CronEvent {
 
 export type CronEventHandler = {
   [CronEvent.CANCEL]: (name: string, id: number) => any;
+  [CronEvent.PRE_START]: (history: CronHistory) => any;
+  [CronEvent.POST_START]: (history: CronHistory) => any;
+  [CronEvent.PRE_RUN]: (history: CronHistory) => any;
+  [CronEvent.POST_RUN]: (history: CronHistory) => any;
+  [CronEvent.SUCCESS]: (history: CronHistory) => any;
+  [CronEvent.FAILED]: (history: CronHistory) => any;
 };
